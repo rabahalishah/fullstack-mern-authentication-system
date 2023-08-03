371ec411d8ccef62d5cec0bb6bdc8bee73aed171
Rabah Ali Shah <muhammadrabah.ali46@gmail.com>
2023-08-03 09:43:58 +0500

    Fixed errors in the header component file

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,4 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
 
 const Header = () => {
@@ -7,22 +6,16 @@ const Header = () => {
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <LinkContainer to='/'>
-            <Navbar.Brand>MERN App</Navbar.Brand>
-          </LinkContainer>
+          <Navbar.Brand href='/'>MERN App</Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <LinkContainer to='/login'>
-                <Nav.Link>
-                  <FaSignInAlt /> Sign In
-                </Nav.Link>
-              </LinkContainer>
-              <LinkContainer to='/register'>
-                <Nav.Link>
-                  <FaSignOutAlt /> Sign Up
-                </Nav.Link>
-              </LinkContainer>
+              <Nav.Link href='/login'>
+                <FaSignInAlt /> Sign In
+              </Nav.Link>
+              <Nav.Link href='/register'>
+                <FaSignOutAlt /> Sign Up
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
