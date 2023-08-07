112ac127cf9dc590f566665209ad52ee5162091f
Rabah Ali Shah <muhammadrabah.ali46@gmail.com>
2023-08-07 11:58:02 +0500

    Added reduc toolkit

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,26 +1,34 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => {
   return (
     <header>
-      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
-        <Container>
-          <Navbar.Brand href='/'>MERN App</Navbar.Brand>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='ms-auto'>
-              <Nav.Link href='/login'>
+    <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+      <Container>
+        <LinkContainer to='/'>
+          <Navbar.Brand>MERN App</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='ms-auto'>
+            <LinkContainer to='/login'>
+              <Nav.Link>
                 <FaSignInAlt /> Sign In
               </Nav.Link>
-              <Nav.Link href='/register'>
+            </LinkContainer>
+            <LinkContainer to='/register'>
+              <Nav.Link>
                 <FaSignOutAlt /> Sign Up
               </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </header>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  </header>
+
   );
 };
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import LoginScreen from "./screens/LoginScreen.jsx";
+import HomeScreen from "./screens/HomeScreen";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import RegisterScreen from "./screens/RegisterScreen.jsx";
+import store from "./store";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
+);
